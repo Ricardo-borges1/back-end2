421c4b3109a137964847654c34d571d020eac1c9
RICARDO BORGES DO AMARAL <23111826@CFP127ALUNO.LOCAL>
2024-02-01 10:34:18 -0300

    banco de dados iniciado

---
+++ controller/controller_filme.js
@@ -0,0 +1,43 @@
+/*******************************************************
+ * Objetivo: arquivo responsável pela validação, consistência de dados das requisições da API de filmes
+ * DATA: 01/02/2024
+ * Autor: Ricardo Borges
+ * Versão: 1.0
+*******************************************************/
+
+
+//função para validar e inserir um novo filme
+const setInserirNovoFilme = async function (){
+
+}
+
+
+//função para validar e atualizar um filme
+const setAtualizarFilme = async function(){
+
+}
+
+//funcção para excluir um filme
+const setExcluirFilme = async function(){
+
+}
+
+//função para retornar todos os filmes
+const getListarFilmes = async function(){
+
+}
+
+//função para buscar filme pelo ID
+const getBuscarFilme = async function (){
+
+}
+
+
+
+module.exports = {
+    setAtualizarFilme,
+    setInserirNovoFilme,
+    setExcluirFilme,
+    getBuscarFilme,
+    getListarFilmes
+}
---
+++ model/DAO/filme.js
@@ -0,0 +1,42 @@
+/*******************************************************
+ * Objetivo: arquivo responsável pelo acesso ao Banco de Dados mySql, aqui faremos o CRUD na tabela de filmes
+ * DATA: 01/02/2024
+ * Autor: Ricardo Borges
+ * Versão: 1.0
+*/
+
+
+// ASYNC OBRIGATORIO
+//funcao para inserir um filme no banco de dados
+const insertFilme = async function(){
+
+}
+
+//funcao para atualizar um filme no banco de dados
+const updateFilme = async function(){
+
+}
+
+//função para excluir um filme no banco de dodos
+const deleteFilme = async function (){
+
+}
+
+//função para listar todos os filmes do banco de dados
+const selectAllFilmes = async function(){
+
+}
+
+// função para buscar um filme no banco de dados filtrando pelo id 
+const selectByIdFilme = async function (){
+
+}
+
+
+module.exports = {
+    insertFilme,
+    updateFilme,
+    deleteFilme,
+    selectAllFilmes,
+    selectByIdFilme
+}
